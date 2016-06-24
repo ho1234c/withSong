364805d2b941ccd56ddf35ece198515ea235bb4b
jongho <ho1234c@gmail.com>
2016-06-24 16:24:24 +0900

    test for mobile

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.route('/')
     // get request
     .get(function(req,res){
         var md = new MobileDetect(req.header['user-agent']);
-
+        console.log(md.os());
+        console.log(md.is('AndroidOS'));
         if(md.is('AndroidOS') || md.is('iOS') || md.is('BlackBerryOS')){
             app.use(express.static(__dirname + '/public/mobile', {index: false}));
             res.sendFile(__dirname + '/public/mobile/index.html');
