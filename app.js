8529d9eb921e011bbe3b09fbef6c494ec3364c17
jongho <ho1234c@gmail.com>
2016-06-24 16:06:27 +0900

    test for mobile

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ app.route('/')
     // get request
     .get(function(req,res){
         var md = new MobileDetect(req.header['user-agent']);
-        console.log(req);
-        console.log(md.mobile());
-        if(md.mobile()){
+        if(md.os()){
             app.use(express.static(__dirname + '/public/mobile', {index: false}));
             res.sendFile(__dirname + '/public/mobile/index.html');
         }
