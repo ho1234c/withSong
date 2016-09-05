b99a508d04c5df5bb97c48389af4596be012f38b
jongho <ho1234c@gmail.com>
2016-09-05 13:07:38 +0900

    fix promise function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express         = require('express');
 var bodyParser      = require('body-parser');
-var MobileDetect = require('mobile-detect');
+var MobileDetect    = require('mobile-detect');
 var models          = require('./models');
 
 var app = express();
@@ -44,44 +44,32 @@ app.route('/')
                 .then(function(user){
                     //check duplicate email.
                     if(!user){
-                        models.User.create({user_email: req.body.email})
-                            .then(function(){
-                                res.send('success');
-                            })
-                            .catch(function(err){
-                                // user.create exception
-                                console.log(err);
-                                res.send('server error');
-                            });
+                        return models.User.create({user_email: req.body.email})
                     }else{
                         res.send('Duplicate email. please use another Email');
                     }
                 })
+                .then(function(){
+                    res.send('success');
+                })
                 .catch(function(err){
-                    // user.find exception
                     console.log(err);
                     res.send('server error');
                 });
-            console.log(req.body);
         }
         // if query string is 'login'
         else if(req.query.type === 'login'){
             models.User.find({ where: {user_email: req.body.email},raw: true })
                 .then(function(user){
-                    // do you have user?
+                    // check register
                     if(user){
-                        models.Song.findAll({ where: {user_id: user.user_id},raw : true})
-                            .then(function(song){
-                                res.json({song: song});
-                            })
-                            .catch(function(err){
-                                // song.find exception
-                                console.log(err);
-                                res.send('server error');
-                            });
+                        return models.Song.findAll({ where: {user_id: user.user_id},raw : true})
                     }else{
                         res.send('Email does not exist.');
                     }
+                })
+                .then(function(song){
+                    res.json({song: song});
                 })
                 .catch(function(err){
                     // user.find exception
@@ -96,15 +84,10 @@ app.route('/')
         var songObj = req.body;
         models.User.find({where: {user_email: songObj.userInfo}, raw: true})
             .then(function(user){
-                models.Song.create({user_id: user.user_id, contents: JSON.stringify(songObj.data), list_name: 'test'})
-                    .then(function(){
-                        res.send('success');
-                    })
-                    .catch(function(err){
-                        // song.create exception
-                        console.log(err);
-                        res.send('server error');
-                    });
+                return models.Song.create({user_id: user.user_id, contents: JSON.stringify(songObj.data), list_name: 'test'})
+            })
+            .then(function(){
+                res.send('success');
             })
             .catch(function(err){
                 // user.find exception
@@ -118,14 +101,10 @@ app.route('/')
         var songObj = req.body;
         models.User.find({where: {user_email: songObj.userInfo}, raw: true})
             .then(function(user){
-                models.Song.destroy({where: {user_id: user.user_id, contents: JSON.stringify(songObj.data)}})
-                    .then(function(){
-                        res.send('success');
-                    })
-                    .catch(function(err){
-                        console.log(err);
-                        res.send('server error');
-                    });
+                return models.Song.destroy({where: {user_id: user.user_id, contents: JSON.stringify(songObj.data)}})
+            })
+            .then(function(){
+                res.send('success');
             })
             .catch(function(err){
                 // user.find exception
